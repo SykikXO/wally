adb06eafd48823d0d9072b80496b5ec0b2e6be87
sykikxo <xo.sykik@gmail.com>
2025-12-23 09:24:44 +0530

    Add custom 404 and 500 error pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from app.extensions import db, migrate, login_manager
+from flask import render_template
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -27,6 +28,16 @@ def create_app(config_class=Config):
     import os
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs(app.config['QUARANTINE_FOLDER'], exist_ok=True)
+
+    # Register error handlers
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('errors/error.html', code=404, title="Page Not Found", message="The wallpaper you're looking for seems to have vanished into the void."), 404
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        db.session.rollback()
+        return render_template('errors/error.html', code=500, title="Server Error", message="Something went wrong on our end. We're dispatching a digital cleanup crew."), 500
 
     return app
 
